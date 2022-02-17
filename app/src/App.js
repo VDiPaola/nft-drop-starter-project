cda724d6c7948577f402450066bdf783a25c2a9b
VDiPaola <dipaolae98@gmail.com>
2022-02-17 14:41:23 +0000

    section 1

--- app/src/App.js
+++ app/src/App.js
@@ -1,27 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
-
-// Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  useEffect(()=>{
+    console.log(
+      'Public Key:',
+      walletAddress
+    );
+  }, [walletAddress])
+
+  const checkIsWalletConnected = async () => {
+    try{
+      const {solana} = window;
+      if(solana && solana.isPhantom){
+        console.log("phantom wallet found")
+        const response = await solana.connect({onlyIfTrusted: true});
+        setWalletAddress(response.publicKey.toString());
+      }else{
+        alert("Download the phantom wallet");
+      }
+    }catch(err){
+      console.error("isWalletConnected: ", err);
+    }
+  }
+
+  const connectWallet = async () => {
+    try{
+      const {solana} = window;
+      if(solana){
+        const response = await solana.connect();
+        setWalletAddress(response.publicKey.toString());
+      }
+    }catch(err){
+      console.error("connectWallet: ", err);
+    }
+  }
+
+  const renderSignInButton = () => (
+        <button
+        className="cta-button connect-wallet-button"
+        onClick={connectWallet}
+      >
+        Connect to Wallet
+      </button>
+    )
+
+  useEffect(()=>{
+    const onLoad = async () => {
+      checkIsWalletConnected();
+    }
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          {!walletAddress && renderSignInButton()}
         </div>
       </div>
     </div>
